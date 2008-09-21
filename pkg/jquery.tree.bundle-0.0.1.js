7b69d67fc23efef1dc6d939e432b3148a4888484
collin <collintmiller@gmail.com>
2008-09-21 01:03:50 -0500

    destroy plugin

--- pkg/jquery.tree.bundle-0.0.1.js
+++ pkg/jquery.tree.bundle-0.0.1.js
@@ -84,7 +84,7 @@
 
 
 ;(jQuery(function() {
-  jQuery("head").append("<style>.tree{font-size:.7em;font-family:sans-serif}.tree .tree_node.dragging{position:absolute;border:1px outset;background-color:white !important;z-index:10000000000}.tree .tree_node.inspected> button.toggle{background-image:url(/icons/close.png)}.tree .tree_node .toggle{border:none;background:none;width:16px;height:16px;display:inline;float:left;position:relative;top:2px;top:4px}.tree .tree_node .toggle.closed{background-image:url(/icons/open.png)}.tree .tree_node.empty > button.toggle{visibility:hidden}.tree ol,.tree ul{list-style:none}.tree ol{white-space:nowrap;background-color:white;padding:0}.tree ol .inspected{background-color:#fcc}.tree ol .inspected .tree_node{background-color:white}.tree ol li{white-space:nowrap;display:block;clear:both;padding-left:10px;margin-left:0px}.tree.inspected> button.toggle{background-image:url(/icons/close.png)}.tree .toggle{border:none;display:inline;position:relative;top:4px;float:left;width:12px;height:12px;background:none;width:16px;height:16px}.tree .toggle.closed{background-image:url(/icons/open.png)}.tree.empty > button.toggle{visibility:hidden}li.inspected> button.disable{background-color:transparent;background-image:url(/icons/block.png)}li button.disable{border:none;display:inline;position:relative;top:4px;float:left;width:12px;height:12px;background:none;margin-right:10px}li button.disable.active{background-image:url(/icons/active_block.png)}</style>");
+  jQuery("head").append("<style>.tree{font-size:.7em;font-family:sans-serif}.tree .tree_node.dragging{position:absolute;border:1px outset;background-color:white !important;z-index:10000000000}.tree .tree_node.inspected> button.toggle{background-image:url(/icons/close.png)}.tree .tree_node .toggle{border:none;background:none;width:16px;height:16px;display:inline;float:left;position:relative;top:2px;top:4px}.tree .tree_node .toggle.closed{background-image:url(/icons/open.png)}.tree .tree_node.empty > button.toggle{visibility:hidden}.tree ol,.tree ul{list-style:none}.tree ol{white-space:nowrap;background-color:white;padding:0}.tree ol .inspected{background-color:#fcc}.tree ol .inspected .tree_node{background-color:white}.tree ol li{white-space:nowrap;display:block;clear:both;padding-left:10px;margin-left:0px}.tree.inspected> button.toggle{background-image:url(/icons/close.png)}.tree .toggle{border:none;display:inline;position:relative;top:4px;float:left;width:12px;height:12px;background:none;width:16px;height:16px}.tree .toggle.closed{background-image:url(/icons/open.png)}.tree.empty > button.toggle{visibility:hidden}li.inspected> button.disable{background-color:transparent;background-image:url(/icons/block.png)}li button.disable{border:none;display:inline;position:relative;top:4px;float:left;width:12px;height:12px;background:none;margin-right:10px}li button.disable.active{background-image:url(/icons/active_block.png)}li.inspected> button.destroy{background-color:transparent;background-image:url(/icons/destroy.png)}li button.destroy{border:none;display:inline;position:relative;top:4px;float:left;width:12px;height:12px;background:none;margin-right:10px;opacity:.5}li button.destroy:hover{opacity:1}</style>");
 }));
 
 jQuery.tree_node = jQuery("<li class='tree_node empty'>  <button class='toggle'></button>  <span>Tree Node</span>  <ol></ol></li>");
@@ -92,6 +92,8 @@ jQuery.tree_node = jQuery("<li class='tree_node empty'>  <button class='toggle'>
 jQuery.toggle_button = jQuery("<button class='toggle'></button>");
 
 jQuery.disable_button = jQuery("<button class='disable'></button>");
+
+jQuery.destroy_button = jQuery("<button class='destroy'></button>");
 
 ;(function(_) {
   var closed_class = 'closed'
@@ -157,6 +159,26 @@ jQuery.disable_button = jQuery("<button class='disable'></button>");
       el.toggleClass(active_class);
       if(el.hasClass(active_class) node.trigger(disable_event);
       else                    node.trigger(enable_event);    
+    }
+  });
+})(jQuery);
+
+
+;(function(_) {
+  var destroy_event = 'destroy'
+
+  _.inject_destroy_dom = function() {
+    _.tree_node.prepend(_.destroy_button]);
+  }
+  
+  _.fn.extend({
+    destroy_button: function() {
+      return this.find('.destroy:first');
+    }
+    
+    ,destroy_click: function(el, node) {
+      node.trigger(destroy_event);
+      node.remove();
     }
   });
 })(jQuery);
