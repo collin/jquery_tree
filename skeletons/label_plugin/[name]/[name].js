de62835221f6065a95324fbe555d8d69b117a1c9
collin <collintmiller@gmail.com>
2008-09-21 01:25:43 -0500

    beginning of label skeleton

---
+++ skeletons/label_plugin/[name]/[name].js
@@ -0,0 +1,15 @@
+;(function(_) {
+  _.inject_<%= name %>_dom = function() {
+    _.tree_node.prepend(_.<%= name %>_button]);
+  }
+  
+  _.fn.extend({
+    <%= name %>_button: function() {
+      return this.find('.<%= name %>:first');
+    }
+    
+    ,<%= name %>_click: function() {
+      
+    }
+  });
+})(jQuery);
