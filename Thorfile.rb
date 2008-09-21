14466abc5a09785029081016846dd11eec1554b2
collin <collintmiller@gmail.com>
2008-09-21 01:20:30 -0500

    shuffled skeletons

--- Thorfile.rb
+++ Thorfile.rb
@@ -7,9 +7,9 @@ module JQuery
   module Tree
     class Gen < Thor
       desc 'plugin', "Generate a named skeleton for a plugin"
-      def plugin(name)
+      def button_plugin(name)
         source_dir = Dir.pwd
-        installer = Basis::Installer.new("#{source_dir}/skeletons/plugin", 
+        installer = Basis::Installer.new("#{source_dir}/skeletons/button_plugin", 
                                     "#{source_dir}/lib/plugins")
         installer.install :name => name
       end    
--- pkg/jquery.tree-0.0.1.js
+++ pkg/jquery.tree-0.0.1.js
@@ -101,11 +101,11 @@ jQuery.destroy_button = jQuery("<button class='destroy'></button>");
 
 ;(function(_){
   _.tree = {
-    plugins: []
+    button_plugins: []
     
-    ,use_plugins: function(names) {
+    ,use_button_plugins: function(names) {
       _(names.split(/ |,/)).each(function() {
-        _.plugins.push(this);
+        _.button_plugins.push(this);
         _["inject_"+this+"_dom]"();
       });
     }
@@ -130,10 +130,67 @@ jQuery.destroy_button = jQuery("<button class='destroy'></button>");
         
         if(el.is('input')) return;
         node.blur_all();
-        _.tree.plugins.each(function() {
+        _.tree.button_plugins.each(function() {
           if(el.hasClass(this)) el[this+'_click'](el, node);
         });
       }        
+    }
+/*
+  label: the jqueried label
+  input: the jqueried input elment
+
+  insertion_method: method to insert the input: 'append', 'before', etc.
+    defaults to 'after'
+  do_not_hide_label: keep the label around so it's css will apply
+  default_value: set the label to this if the value is ""
+  hide_if_empty: hide the label if the value is ""
+  remove_if_empty: remove the label if the value is ""
+*/
+    ,edit_label: function(opts) {
+      var label = opts.label
+        ,input = opts.input;
+
+      _(document.body).blur_all();
+      input.val(label.text());
+      
+      if(opts.do_not_hide_label) label.clear().css('display', '');
+      else label.hide();
+      
+      label[opts.insertion_method || 'after'](input.show());
+     
+      input
+        .size_to_fit()
+        .one('blur', function() {
+          _(document.body).append(input.hide());
+          if(opts.default_value) {
+            label
+              .html(input.val() || opts.default_value)
+              .show();
+          }
+          else if(opts.hide_if_empty) {      
+            label
+              .html(input.val())
+              .hide_if_empty();
+          }
+          else if(opts.remove_if_empty) {
+            label
+              .html(input.val())
+              .remove_if_empty();
+          }
+          else if(opts.if_empty) {
+            label
+              .html(input.val())
+              .if_empty(opts.if_empty);
+          }
+          else {
+            label.html(input.val());
+          }
+        });
+      
+      label.parent().length && opts.success && opts.success.call(label);
+      
+      setTimeout(function(){input.focus();}, 1);
+      return this;
     }
   });
 })(jQuery);
