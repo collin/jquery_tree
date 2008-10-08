d6c2f6e43f56fd96a8f83faa195e916c5733a216
collin <collintmiller@gmail.com>
2008-10-08 18:34:49 -0500

    node type generator, generated code node

---
+++ skeletons/node_type_plugin/[name]/[name].js
@@ -0,0 +1,11 @@
+;(function(_) {
+  _.tree.<%= name %>.fn({
+    edit: function() {
+      var _this = _(this);
+    }
+    
+    ,paint: function(data) {
+      var _this = _(this)
+    }
+  });
+})(jQuery);
